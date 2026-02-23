e3c6ec6f248c2dd388ec719c7124451f19c3610e
Sarthak Singh <mic.sarthaksingh9b@example.com>
2026-02-23 23:19:28 +0530

    d

---
+++ cript.js
@@ -0,0 +1,15 @@
+console.log("Hello, World!");
+var a=10;
+console.log(a)
+var b = 20;
+var sum = a + b;
+console.log("The sum of a and b is: ", sum);
+
+console.log("Let introduction");
+let d=10;
+d=20;
+console.log(d);
+
+const x="100";
+console.log(x);
+
