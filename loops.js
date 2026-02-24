fe9c2d15a0167da117cf83f1709b5e7bf2e48e03
Sarthak Singh <mic.sarthaksingh9b@example.com>
2026-02-24 21:13:30 +0530

    edit

--- loops.js
+++ loops.js
@@ -1,3 +1,19 @@
-while (true){
-    console.log("Hello");
+for (let i = 1; i <= 5; i++) {
+  console.log("Iteration number: " + i);
 }
+
+
+let count = 1;
+
+while (count <= 5) {
+  console.log("Count is: " + count);
+  count++;
+}
+
+
+let energy = 0;
+
+do {
+  console.log("Working..."); 
+  // This runs even though energy is 0
+} while (energy > 0);
